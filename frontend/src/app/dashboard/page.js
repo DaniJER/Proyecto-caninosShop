c345e6f5080f745cb3951f3cd9789da1e5ab4449
Jhonni10 <jhonieripianoscue@gmail.com>
2024-10-21 20:01:21 -0500

    added dashboard and config of mui

---
+++ frontend/src/app/dashboard/page.js
@@ -0,0 +1,8 @@
+
+export default function DashboardPage() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      dashboard
+    </div>
+  );
+}
---
+++ frontend/src/components/dashboard/themes/index.js
@@ -0,0 +1,11 @@
+'use client';
+
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'var(--font-roboto)',
+  },
+});
+
+export default theme;
