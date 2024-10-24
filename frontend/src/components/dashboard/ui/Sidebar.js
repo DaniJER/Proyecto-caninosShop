ef0dafd2c0927b6c5e180b2a373bd0ccf14dc4e3
Jhonni10 <jhonieripianoscue@gmail.com>
2024-10-24 15:08:09 -0500

    Added StyledAccount component and updated Sidebar component with new account information display

--- frontend/src/components/dashboard/ui/Sidebar.js
+++ frontend/src/components/dashboard/ui/Sidebar.js
@@ -1,9 +1,19 @@
 'use client'
-import { Box, Drawer, IconButton, Toolbar, Typography } from '@mui/material'
+import { alpha, Avatar, Box, Drawer, IconButton, Link, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import useResponsive from '../hooks/useResponsive'
 import Image from 'next/image'
 import { IoIosCloseCircleOutline } from "react-icons/io";
+import styled from '@emotion/styled'
+
+const StyledAccount = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(2, 2.5),
+    borderRadius: Number(theme.shape.borderRadius) * 1.5,
+    backgroundColor: alpha(theme.palette.grey[500], 0.12),
+}));
+
 
 const Sidebar = ({draweWidth, openNav, onCloseNav, onToggleDrawerWidth}) => {
 
@@ -39,6 +49,35 @@ const Sidebar = ({draweWidth, openNav, onCloseNav, onToggleDrawerWidth}) => {
                     
                 </IconButton>
             </Toolbar>
+            <Box sx={{ 
+                    mb: draweWidth === 88 ? 1 : 3, 
+                    mx:  draweWidth === 88 ? 1: 2.5, 
+                    mt: draweWidth === 88 ? 1: 2, 
+                }}>
+                <Link underline="none">
+                <StyledAccount sx={{
+                    flexDirection: draweWidth === 88 ? 'column':'row',
+
+                }}>
+                    <Avatar src={'https://inversionesdiomardisas.vercel.app/assets/images/avatars/avatar_default.jpg'} alt="photoURL" />
+
+                    <Box sx={{ ml:draweWidth === 88 ? 0 : 1 }}>
+                    <Typography 
+                        variant="subtitle2" 
+                        sx={{ 
+                            color: 'text.primary',
+                            fontWeight:'700',
+                            ...(draweWidth === 88 && {
+                                lineHeight:'2.2',
+                                fontSize:'0.76rem',
+                            }),
+                            }}>
+                        name
+                    </Typography>
+                    </Box>
+                </StyledAccount>
+                </Link>
+            </Box>
         </>
     )
 
