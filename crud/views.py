63477e75ce88aadcb2e818c08d2f9821a43ed521
Daniel Enriquez <danicraft.op7@gmail.com>
2024-11-04 18:46:23 -0600

    api para la creacion de un usuario

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import CrearUsuario
+from .serializers import UsuarioSerializer
 # Create your views here.
+
+
+class UsuarioNuevoSet(viewsets.ModelViewSet):
+    queryset = CrearUsuario.objects.all()
+    serializer_class = UsuarioSerializer
