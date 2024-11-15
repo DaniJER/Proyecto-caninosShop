ce7e85199f628f75156d66366faccebf7306925f
Jhonni10 <jhonieripianoscue@gmail.com>
2024-11-15 11:26:04 -0500

    Refactor PetsApp and UserApp components, add sorting and table headers, remove UserListHead component

--- frontend/src/components/dashboard/ui/secctions/pets/PetsApp.js
+++ frontend/src/components/dashboard/ui/secctions/pets/PetsApp.js
@@ -1,12 +1,37 @@
-import { Button, Card, Container, Stack, Typography } from '@mui/material'
+'use client';
+
+import { Button, Card, Container, Stack, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material'
 import { FaPlus } from "react-icons/fa6";
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { PetsListToolbar } from './PetsListToolbar';
+import ScrollBar from '../../scrollbar/ScrollBar';
+import ListHead from '../ui/ListHead';
+
+const TABLE_HEAD = [
+    { id: 'pet', label: 'Mascota', alignRight: false },
+    { id: 'createAt', label: 'Creado en', alignRight: false },
+    { id: 'price', label: 'Precio', alignRight: false },
+    { id: 'stock', label: 'Existencias', alignRight: false },
+    { id: '' },
+  ];
+
+const PRODUCTS_LIST = []
 
 
 
 export const PetsApp = () => {
+
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('pet');
+
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+      };
+
   return (
     <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={4} mt={2}>
@@ -22,6 +47,35 @@ export const PetsApp = () => {
 
         <Card>
             <PetsListToolbar/>
+            <ScrollBar>
+                <TableContainer sx={{ minWidth: 800 }}>
+                    <Table>
+                        <ListHead
+                            order={order}
+                            orderBy={orderBy}
+                            headLabel={TABLE_HEAD}
+                            onRequestSort={handleRequestSort}
+                        />
+                        <TableBody>
+                            {/* TODO: MOSTRAR MASCOTAS */}
+                        </TableBody>
+                        {
+                            PRODUCTS_LIST.length <= 0 && (
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell colSpan={6} align="center">
+                                            <Typography variant="h6" color="textSecondary" noWrap p={6}>
+                                                No hay mascotas registradas
+                                            </Typography>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            ) 
+                        }
+                    </Table>
+                </TableContainer>
+            </ScrollBar>
+
         </Card>
     </Container>
   )
